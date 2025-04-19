347228ccfe422538f5263aa2aca982b416bb61db
JyrwaJR <jyrwaboys@gmail.com>
2025-04-19 09:53:10 +0530

    created rest file with env

--- src/utils/validation/auth/index.ts
+++ src/utils/validation/auth/index.ts
@@ -1,7 +1,10 @@
 import { z } from 'zod';
 
 export const authSchema = z.object({
-  phone: z.string({ required_error: 'Phone is required' }),
+  phone: z
+    .string({ required_error: 'Phone is required' })
+    .min(10, { message: 'Min 10 Required' })
+    .max(10, { message: 'Max 10 Required' }),
   password: z
     .string({ required_error: 'Password is required' })
     .min(8, { message: 'Min 8 Required' })
