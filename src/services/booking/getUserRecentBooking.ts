0d591fb4592b545221e708ddafdd78ca5850cb20
JyrwaJR <jyrwaboys@gmail.com>
2025-04-27 14:57:32 +0530

    include parkinglot

--- src/services/booking/getUserRecentBooking.ts
+++ src/services/booking/getUserRecentBooking.ts
@@ -4,6 +4,7 @@ export async function getUserRecentBooking({ id }: { id: string }) {
   return await prisma.booking.findMany({
     where: { userId: id, bookingStatus: { not: 'CANCELLED' } },
     orderBy: { createdAt: 'desc' },
+    include: { parkingLot: true },
     take: 5,
   });
 }
